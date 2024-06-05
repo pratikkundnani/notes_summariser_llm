0b679dac897039461e36d46a7412e3fed9b969c8
pratikkundnani <kundnanipratik@gmail.com>
2024-06-05 11:44:05 +0530

    add summarising using ollama llms

--- app_ollama.py
+++ app_ollama.py
@@ -1,12 +1,11 @@
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
 import streamlit as st
 import os 
 from dotenv import load_dotenv
 
-load_dotenv()
-os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"]=os.getenv("LANGCHAIN_TRACING_V2")
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
@@ -17,10 +16,10 @@ promt = ChatPromptTemplate.from_messages(
     ]
 )
 
-st.title("Notes Summariser App using ChatGPT") 
+st.title("Notes Summariser App using Ollama") 
 input_text=st.text_input("Paste the content to summarise")
 
-llm=ChatOpenAI(model='gpt-3.5-turbo')
+llm=Ollama(model='llama3')
 output_parser=StrOutputParser()
 
 chain= promt | llm | output_parser
