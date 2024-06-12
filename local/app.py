908f95be09ca9f4370a405f43d47183cd277b5f2
pratikkundnani <kundnanipratik@gmail.com>
2024-06-12 21:05:31 +0530

    refactor

--- local/app.py
+++ local/app.py
@@ -9,7 +9,7 @@ load_dotenv()
 os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"]=os.getenv("LANGCHAIN_TRACING_V2")
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-
+print(os.environ["OPENAI_API_KEY"])
 promt = ChatPromptTemplate.from_messages(
     [
         ("system", "You are a document summariser. Please summarise the document keeping the summary short and to the point"),
@@ -17,21 +17,7 @@ promt = ChatPromptTemplate.from_messages(
     ]
 )
 
-st.title("Notes Summariser App using Ollama") 
-st.markdown(
-    f"""
-    <style>
-    .reportview-container .main .block-container{{
-        max-width: 1000px;
-    }}
-    .reportview-container .main .block-container .stTextInput {{
-        resize: vertical;
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-input_text=st.text_area("Paste the content to summarise")
+
 
 
 llm=ChatOpenAI(model='gpt-3.5-turbo')
@@ -55,6 +41,25 @@ def summarize_text(input_text):
         summary = response.invoke({'question': segment})
         summaries.append(summary)
     return " ".join(summaries)
+
+
+
+
+st.title("Notes Summariser App using chatGPT") 
+st.markdown(
+    f"""
+    <style>
+    .reportview-container .main .block-container{{
+        max-width: 1000px;
+    }}
+    .reportview-container .main .block-container .stTextInput {{
+        resize: vertical;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+input_text=st.text_area("Paste the content to summarise")
 
 if st.button('Summarize'):
     st.write(summarize_text(input_text))
